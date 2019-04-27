72ee61ab3863bde5bbb48dcf15f4a0222cf8c3cf
赤坂勝哉 <akasakatora1208@gmail.com>
2019-04-28 00:39:54 +0900

    add: tab and alarm

--- background.js
+++ background.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+const qHeader = "https://connpass.com/api/v1/event/?"
+chrome.browserAction.onClicked.addListener(
+    () => {
+        chrome.tabs.create({ url: './setting.html' });
+    }
+);
+
+chrome.alarms.create('call', { 'periodInMinutes': 10 });
+chrome.alarms.onAlarm.addListener(alarm => {
+    if (alarm.name === 'call') {
+        // localStorageからqueryを取得する
+        const query = getQuery();
+        // post qHeader + query
+        const parsedRes = parseResponse()
+        noti(parsedRes)
+    }
+});
+
+
+function getQuery() {
+    return "hoge"
+}
+
+function parseResponse() {
+    return "hogehoge"
+}
